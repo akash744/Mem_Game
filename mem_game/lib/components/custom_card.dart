46b05015779759cbfc372d6cc02b70f2ed451c03
akash744 <akashashok12341999@gmail.com>
2022-08-01 21:16:39 +1200

    updated ui

--- mem_game/lib/components/custom_card.dart
+++ mem_game/lib/components/custom_card.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
 
-Widget CustomCard(String title, String info) {
+Widget CustomCard(String title, String value) {
   return Expanded(
-    child: Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Column(
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
+      child: Container(
+    margin: EdgeInsets.all(15.0),
+    decoration: BoxDecoration(
+      color: Color(0xFF3A405A),
+    ),
+    child: Column(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
-          const SizedBox(
-            height: 6.0,
+        ),
+        const SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 50.0,
+            fontWeight: FontWeight.w900,
+            color: Colors.white,
           ),
-          Text(
-            info,
-            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-    )
-  );
+        ),
+      ],
+    ),
+  ));
 }
--- mem_game/lib/main.dart
+++ mem_game/lib/main.dart
@@ -41,11 +41,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF0f4d19),
+      backgroundColor: Color(0xFF3A405A),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          const SizedBox(
+            height: 30,
+          ),
           const Center(
             child: Text(
               "Mem Game",
@@ -53,6 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 fontSize: 40.0,
                 fontWeight: FontWeight.bold,
                 color: Colors.white,
+                fontFamily: 'Pacifico',
               ),
             ),
           ),
@@ -111,7 +115,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         child: Container(
                           padding: EdgeInsets.all(16.0),
                           decoration: BoxDecoration(
-                            color: Color(0xFF6fc27c),
+                            color: Color(0xFF99B2DD),
                             borderRadius: BorderRadius.circular(12.0),
                             image: DecorationImage(
                               image: AssetImage(_game.cardImgList![index]),
